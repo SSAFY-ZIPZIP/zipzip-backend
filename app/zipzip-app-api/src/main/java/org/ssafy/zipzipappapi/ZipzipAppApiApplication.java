becae11d207a36d9c8b56ec8544ad997e36573d3
king0104 <king0104@konkuk.ac.kr>
2024-11-04 15:11:53 +0900

    init: multi module setting

---
+++ app/zipzip-app-api/src/main/java/org/ssafy/zipzipappapi/ZipzipAppApiApplication.java
@@ -0,0 +1,12 @@
+package org.ssafy.zipzipappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipzipAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipzipAppApiApplication.class, args);
+	}
+}
---
+++ app/zipzip-app-api/src/test/java/org/ssafy/zipzipappapi/ZipzipAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ssafy.zipzipappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipzipAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
