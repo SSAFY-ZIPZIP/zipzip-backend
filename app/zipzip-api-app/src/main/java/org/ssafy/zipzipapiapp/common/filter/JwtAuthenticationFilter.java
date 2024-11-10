050899f9e87cf5a24456b6d84c931148c1008b5c
yeseul106 <20191037@sungshin.ac.kr>
2024-11-10 15:54:02 +0900

    #5 refactor: 인증 관련 필터 코드 리팩토링

--- app/zipzip-api-app/src/main/java/org/ssafy/zipzipapiapp/common/filter/JwtAuthenticationFilter.java
+++ app/zipzip-api-app/src/main/java/org/ssafy/zipzipapiapp/common/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,7 @@
 package org.ssafy.zipzipapiapp.common.filter;
 
-import static org.ssafy.zipzipexceptioncommon.exception.ErrorMessage.ERR_ACCESS_TOKEN_EXPIRED;
-import static org.ssafy.zipzipexceptioncommon.exception.ErrorMessage.ERR_REFRESH_TOKEN_EXPIRED;
-import static org.ssafy.zipzipexceptioncommon.exception.ErrorMessage.ERR_UNAUTORIZED;
-import static org.ssafy.zipzipapiapp.common.jwt.JwtExceptionType.EMPTY_JWT;
-import static org.ssafy.zipzipapiapp.common.jwt.JwtExceptionType.EXPIRED_JWT_TOKEN;
-import static org.ssafy.zipzipapiapp.common.jwt.JwtExceptionType.VALID_JWT_TOKEN;
+import static org.ssafy.zipzipexceptioncommon.exception.ErrorMessage.ERR_EMPTY_TOKEN;
+import static org.ssafy.zipzipexceptioncommon.exception.ErrorMessage.ERR_INVALID_TOKEN;
 
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
@@ -20,84 +16,87 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.ssafy.zipzipexceptioncommon.exception.UnAuthorizedException;
 import org.ssafy.zipzipapiapp.common.jwt.JwtAuthenticationEntryPoint;
 import org.ssafy.zipzipapiapp.common.jwt.JwtExceptionType;
 import org.ssafy.zipzipapiapp.common.jwt.JwtTokenProvider;
+import org.ssafy.zipzipapiapp.common.jwt.TokenType;
 import org.ssafy.zipzipapiapp.common.jwt.UserAuthentication;
+import org.ssafy.zipzipexceptioncommon.exception.UnAuthorizedException;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
     private static final String REISSUE_API_URL = "/v1/oauth/reissue";
     private static final String LOGOUT_API_URL = "/v1/oauth/logout";
+    private static final String LOGIN_API_URL = "/v1/oauth/login";
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
-        try {
-            String accessToken = jwtTokenProvider.resolveAccessToken(request);
-            if (REISSUE_API_URL.equals(request.getRequestURI())) {
-                handleTokenReissue(request);
-            } else if (LOGOUT_API_URL.equals(request.getRequestURI())) {
-                handleLogout(request);
-            } else {
-                validateAccessToken(accessToken);
-            }
-        } catch (UnAuthorizedException e) {
-            jwtAuthenticationEntryPoint.setResponse(response, HttpStatus.UNAUTHORIZED, e.getMessage());
+
+        // 로그인의 경우, accessToken으로 검증할 필요 없음
+        if (isLoginRequest(request)) {
+            chain.doFilter(request, response);
             return;
         }
 
-        chain.doFilter(request, response);
+        try {
+            if (isTokenReissueOrLogoutRequest(request)) {
+                handleRefreshTokenValidation(request);
+            } else {
+                handleAccessTokenValidation(request);
+            }
+            chain.doFilter(request, response);
+        } catch (UnAuthorizedException e) {
+            jwtAuthenticationEntryPoint.sendErrorResponse(response, HttpStatus.UNAUTHORIZED, e.getErrorMessage());
+        }
     }
 
-    private void handleTokenReissue(HttpServletRequest request) {
-        String refreshToken = jwtTokenProvider.resolveRefreshToken(request);
-        validateRefreshToken(refreshToken);
-        Long memberId = jwtTokenProvider.validateMemberRefreshToken(refreshToken);
-        String newAccessToken = jwtTokenProvider.generateAccessToken(memberId);
-
-        setAuthentication(newAccessToken);
-        request.setAttribute("newAccessToken", newAccessToken);
+    private boolean isLoginRequest(HttpServletRequest request) {
+        String requestUri = request.getRequestURI();
+        return LOGIN_API_URL.equals(requestUri);
     }
 
-    private void handleLogout(HttpServletRequest request) {
-        String refreshToken = jwtTokenProvider.resolveRefreshToken(request);
+    private boolean isTokenReissueOrLogoutRequest(HttpServletRequest request) {
+        String requestUri = request.getRequestURI();
+        return REISSUE_API_URL.equals(requestUri) || LOGOUT_API_URL.equals(requestUri);
+    }
 
+    private void handleAccessTokenValidation(HttpServletRequest request) {
+        String accessToken = jwtTokenProvider.resolveAccessToken(request);
+        validateToken(accessToken, TokenType.ACCESS);
+        setAuthentication(accessToken);
+    }
+
+    private void handleRefreshTokenValidation(HttpServletRequest request) {
+        String refreshToken = jwtTokenProvider.resolveRefreshToken(request);
+        validateToken(refreshToken, TokenType.REFRESH);
         request.setAttribute("refreshToken", refreshToken);
     }
 
+    private void validateToken(String token, TokenType tokenType) {
+        JwtExceptionType jwtException = jwtTokenProvider.validateToken(token);
 
-    private void validateRefreshToken(String refreshToken) {
-        JwtExceptionType jwtException = jwtTokenProvider.validateToken(refreshToken);
-        if (jwtException == EMPTY_JWT) {
-            throw new UnAuthorizedException(ERR_UNAUTORIZED);
+        if (jwtException == JwtExceptionType.VALID_JWT_TOKEN) {
+            return;
         }
-        if (jwtException == EXPIRED_JWT_TOKEN) {
-            throw new UnAuthorizedException(ERR_REFRESH_TOKEN_EXPIRED);
-        }
-    }
 
-    private void validateAccessToken(String accessToken) {
-        if (accessToken != null) {
-            JwtExceptionType jwtException = jwtTokenProvider.validateToken(accessToken);
-            if (jwtException == VALID_JWT_TOKEN) {
-                setAuthentication(accessToken);
-            } else if (jwtException == EXPIRED_JWT_TOKEN) {
-                throw new UnAuthorizedException(ERR_ACCESS_TOKEN_EXPIRED);
-            }
+        switch (jwtException) {
+            case EMPTY_JWT -> throw new UnAuthorizedException(ERR_EMPTY_TOKEN);
+            case EXPIRED_JWT_TOKEN -> throw new UnAuthorizedException(tokenType.getExpiredMessage());
+            case INVALID_JWT_TOKEN -> throw new UnAuthorizedException(ERR_INVALID_TOKEN);
         }
     }
 
     private void setAuthentication(String token) {
         Claims claims = jwtTokenProvider.getAccessTokenPayload(token);
-        Authentication authentication = new UserAuthentication(Long.valueOf(String.valueOf(claims.get("id"))), null, null);
+        Long userId = Long.valueOf(String.valueOf(claims.get("id")));
+        Authentication authentication = new UserAuthentication(userId, null, null);
         SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
