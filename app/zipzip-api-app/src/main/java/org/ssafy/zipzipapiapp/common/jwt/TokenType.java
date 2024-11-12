d8dd1b0b0ecefd99452ed6a87e7f0c994793a090
yeseul106 <20191037@sungshin.ac.kr>
2024-11-12 12:05:11 +0900

    #5 refactor: TokenType Enum 클래스 가독성 보수

--- app/zipzip-api-app/src/main/java/org/ssafy/zipzipapiapp/common/jwt/TokenType.java
+++ app/zipzip-api-app/src/main/java/org/ssafy/zipzipapiapp/common/jwt/TokenType.java
@@ -3,21 +3,16 @@ package org.ssafy.zipzipapiapp.common.jwt;
 import static org.ssafy.zipzipexceptioncommon.exception.ErrorMessage.ERR_ACCESS_TOKEN_EXPIRED;
 import static org.ssafy.zipzipexceptioncommon.exception.ErrorMessage.ERR_REFRESH_TOKEN_EXPIRED;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.ssafy.zipzipexceptioncommon.exception.ErrorMessage;
 
+@Getter
+@RequiredArgsConstructor
 public enum TokenType {
-    ACCESS {
-        @Override
-        public ErrorMessage getExpiredMessage() {
-            return ERR_ACCESS_TOKEN_EXPIRED;
-        }
-    },
-    REFRESH {
-        @Override
-        public ErrorMessage getExpiredMessage() {
-            return ERR_REFRESH_TOKEN_EXPIRED;
-        }
-    };
 
-    public abstract ErrorMessage getExpiredMessage();
+    ACCESS(ERR_ACCESS_TOKEN_EXPIRED),
+    REFRESH(ERR_REFRESH_TOKEN_EXPIRED);
+
+    private final ErrorMessage expiredMessage;
 }
