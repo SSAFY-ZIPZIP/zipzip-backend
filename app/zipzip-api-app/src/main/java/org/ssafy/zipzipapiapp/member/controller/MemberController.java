6214e1c01bf8fadd007299add0bb9806d7e7ba20
king0104 <king0104@konkuk.ac.kr>
2024-11-12 19:44:34 +0900

    #9 fix: controller의 getMemberProfile 변경

--- app/zipzip-api-app/src/main/java/org/ssafy/zipzipapiapp/member/controller/MemberController.java
+++ app/zipzip-api-app/src/main/java/org/ssafy/zipzipapiapp/member/controller/MemberController.java
@@ -1,9 +1,12 @@
 package org.ssafy.zipzipapiapp.member.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.ssafy.zipzipapiapp.member.dto.GetMemberProfileResponse;
 import org.ssafy.zipzipapiapp.member.service.MemberSerivce;
@@ -15,7 +18,9 @@ public class MemberController {
     private final MemberSerivce memberSerivce;
 
     @GetMapping("/{memberId}/profile")
-    public GetMemberProfileResponse getMemberProfile(@PathVariable("memberId") Long memberId) {
-        return memberSerivce.getMemberProfile(memberId);
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<GetMemberProfileResponse> getMemberProfile(@PathVariable("memberId") Long memberId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(memberSerivce.getMemberProfile(memberId));
     }
 }
