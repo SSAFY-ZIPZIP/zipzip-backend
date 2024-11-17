ab0267e2c1f7bd643c03fba3bc54bf6bd7baa945
yeseul106 <20191037@sungshin.ac.kr>
2024-11-17 21:31:57 +0900

    #16 feat: 청약에 대한 내 정보 조회 API 개발

--- domain/zipzip-mysql-domain/src/main/java/org/ssafy/zipzipmysqldomain/subscriptionProfile/repository/SubscriptionProfileJpaRepository.java
+++ domain/zipzip-mysql-domain/src/main/java/org/ssafy/zipzipmysqldomain/subscriptionProfile/repository/SubscriptionProfileJpaRepository.java
@@ -1,7 +1,9 @@
 package org.ssafy.zipzipmysqldomain.subscriptionProfile.repository;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.ssafy.zipzipmysqldomain.subscriptionProfile.entity.SubscriptionProfile;
 
 public interface SubscriptionProfileJpaRepository extends CrudRepository<SubscriptionProfile, Long> {
+    Optional<SubscriptionProfile> findByMemberId(Long memberId);
 }
