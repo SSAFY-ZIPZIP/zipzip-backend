ce6e2bd49cbdd19bf02d28a6771db8a8fa3f5b64
yeseul106 <20191037@sungshin.ac.kr>
2024-11-17 18:25:25 +0900

    #13 feat: 청약에 관한 내 정보 입력 API 개발

--- app/zipzip-api-app/src/main/java/org/ssafy/zipzipapiapp/common/util/MemberUtil.java
+++ app/zipzip-api-app/src/main/java/org/ssafy/zipzipapiapp/common/util/MemberUtil.java
@@ -1,17 +1,18 @@
 package org.ssafy.zipzipapiapp.common.util;
+
 import static org.ssafy.zipzipexceptioncommon.exception.ErrorMessage.ERR_UNAUTORIZED;
 
-import java.security.Principal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.ssafy.zipzipexceptioncommon.exception.UnAuthorizedException;
 
 @RequiredArgsConstructor
 public class MemberUtil {
 
-    public static Long getUserId(Principal principal) {
-        if (principal == null) {
+    public static Long getUserId(Authentication authentication) {
+        if (authentication == null) {
             throw new UnAuthorizedException(ERR_UNAUTORIZED);
         }
-        return Long.valueOf(principal.getName());
+        return Long.valueOf(authentication.getName());
     }
 }
---
+++ app/zipzip-api-app/src/main/java/org/ssafy/zipzipapiapp/subscriptionProfile/service/SubscriptionProfileService.java
@@ -0,0 +1,28 @@
+package org.ssafy.zipzipapiapp.subscriptionProfile.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.ssafy.zipzipapiapp.subscriptionProfile.dto.PostSubscriptionProfileRequest;
+import org.ssafy.zipzipmysqldomain.subscription.enums.SubscriptionCategory;
+import org.ssafy.zipzipmysqldomain.subscription.enums.SubscriptionRegion;
+import org.ssafy.zipzipmysqldomain.subscriptionProfile.entity.SubscriptionProfile;
+import org.ssafy.zipzipmysqldomain.subscriptionProfile.repository.SubscriptionProfileRepository;
+
+@Service
+@RequiredArgsConstructor
+public class SubscriptionProfileService {
+
+    private final SubscriptionProfileRepository subscriptionProfileRepository;
+
+    @Transactional
+    public void postSubscriptionProfile(PostSubscriptionProfileRequest postSubscriptionProfileRequest, Long memberId) {
+        SubscriptionProfile newSubscriptionProfile = SubscriptionProfile.builder()
+                .memberId(memberId)
+                .memberCategory(SubscriptionCategory.findByDescription(postSubscriptionProfileRequest.memberCategory()))
+                .memberRegion(SubscriptionRegion.findByDescription(postSubscriptionProfileRequest.memberRegion()))
+                .isNotificationSubscription(false)
+                .build();
+        subscriptionProfileRepository.save(newSubscriptionProfile);
+    }
+}
