57fdcc9561d96dca0d0fd41aafac0f663cbda4fb
king0104 <king0104@konkuk.ac.kr>
2024-11-18 15:53:57 +0900

    #14 fix: 우리zip 추가 API pr 확인 후 2차 수정

--- app/zipzip-api-app/src/main/java/org/ssafy/zipzipapiapp/workspace/dto/PostWorkspaceRequest.java
+++ app/zipzip-api-app/src/main/java/org/ssafy/zipzipapiapp/workspace/dto/PostWorkspaceRequest.java
@@ -4,6 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record PostWorkspaceRequest(
-        @NotBlank(message = "workspace의 이름은 1자 이상 20자 이하입니다") @Size(min = 1, max = 20) String workspaceName
+        @NotBlank(message = "workspace의 이름은 null, \"\". \" \" 이면 안됩니다") @Size(min = 1, max = 20, message = "workspace의 이름은 1자 이상 20자 이하입니다") String workspaceName
 ) {
 }
