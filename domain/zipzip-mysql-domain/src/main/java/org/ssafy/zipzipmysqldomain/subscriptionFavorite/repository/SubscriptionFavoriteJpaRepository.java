f9c9dd7cb63bdc32b5be3f5debbaf83ca358878d
yeseul106 <20191037@sungshin.ac.kr>
2024-11-19 21:59:55 +0900

    #30 feat: 관심 청약 등록 API 개발

---
+++ domain/zipzip-mysql-domain/src/main/java/org/ssafy/zipzipmysqldomain/subscriptionFavorite/repository/SubscriptionFavoriteJpaRepository.java
@@ -0,0 +1,7 @@
+package org.ssafy.zipzipmysqldomain.subscriptionFavorite.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.ssafy.zipzipmysqldomain.subscriptionFavorite.entity.SubscriptionFavorite;
+
+public interface SubscriptionFavoriteJpaRepository extends CrudRepository<SubscriptionFavorite, Long> {
+}
